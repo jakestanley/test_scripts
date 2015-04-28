8889179735a66741615af89fb7bda8d69f8dd0fe
Jake Stanley <madstanners@icloud.com>
2015-04-28 14:00:39 +0100

    added submit flags

--- Resident_2.sh
+++ Resident_2.sh
@@ -1,1 +1,1 @@
-java -jar ../Client_Resident.jar -ORBInitialPort 1713 -fname Robyn -lname Fisherman -email torbay_girl@coolmail.com -location Torbay
+java -jar ../Client_Resident.jar -ORBInitialPort 1713 -fname Robyn -lname Fisherman -email torbay_girl@coolmail.com -location Torbay -submit
